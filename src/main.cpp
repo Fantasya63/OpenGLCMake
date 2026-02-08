80017ad952256b6efb7581aef8489e8e9bf7a8b0
Jansen Baloaloa <87849403+Fantasya63@users.noreply.github.com>
2026-02-08 23:47:32 +0800

    Added UUID

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ int main(void)
         
         void main()
         {
-            FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            FragColor = vec4(0.0, 0.0, 0.0, 1.0);
         }    
     )";
 
